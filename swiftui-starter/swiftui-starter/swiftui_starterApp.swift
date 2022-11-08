bb390caa0bbcb544d86d0ce165d2ebf1ecc2dbf7
Cassie Wallace <cassbwallace@gmail.com>
2022-11-08 15:44:31 -0700

    Initial project setup

---
+++ swiftui-starter/swiftui-starter/swiftui_starterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_starterApp.swift
+//  swiftui-starter
+//
+//  Created by Cassie Wallace on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_starterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

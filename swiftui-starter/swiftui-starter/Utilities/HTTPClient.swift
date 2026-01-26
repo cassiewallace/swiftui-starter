d91fd7c9a4f999e49ac40f0b153debaa168f35c4
Cassie Wallace <cassiewallace@gmail.com>
2026-01-26 10:01:54 -0700

    Switch to Swift Concurrency

--- swiftui-starter/swiftui-starter/Utilities/HTTPClient.swift
+++ swiftui-starter/swiftui-starter/Utilities/HTTPClient.swift
@@ -7,6 +7,13 @@
 
 import Foundation
 
+enum HTTPError: Error {
+    case invalidURL
+    case invalidResponse
+    case encodingError(Error)
+    case decodingError(Error)
+}
+
 enum HTTPMethods: String {
     case DELETE
     case GET
@@ -23,31 +30,25 @@ class HTTPClient {
     // Assumes:
     // 1) The response uses snake_case.
     // 2) Authorization is provided via an API key from Constants.
-    static func get<T: Decodable>(url: String, completionHandler: @escaping (T?) -> Void) {
-        guard let url = URL(string: url) else { return }
+    static func get<T: Decodable>(url: String) async throws -> T {
+        guard let url = URL(string: url) else {
+            throw HTTPError.invalidURL
+        }
         
         var request = URLRequest(url: url)
         request.httpMethod = HTTPMethods.GET.rawValue
         request.setValue(Constants.API.apiKey, forHTTPHeaderField: "Authorization")
-            
-        let sharedSession = URLSession.shared
+
+        let (data, response) = try await URLSession.shared.data(for: request)
         
-        let dataTask = sharedSession.dataTask(with: request) {
-            (data, response, error) in
-                do {
-                    if let jsonData = data {
-                        let decoder = JSONDecoder()
-                        decoder.keyDecodingStrategy = .convertFromSnakeCase
-                        let typedObject: T? = try decoder.decode(T.self, from: jsonData)
-                        completionHandler(typedObject)
-                    }
-                }
-                catch {
-                    print(error)
-                }
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else {
+            throw HTTPError.invalidResponse
         }
         
-        dataTask.resume()
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode(T.self, from: data)
     }
     
     // Make a POST request to a URL.
@@ -55,45 +56,30 @@ class HTTPClient {
     // 1) The request body uses snake_case.
     // 2) The response uses snake_case.
     // 3) Authorization is provided via an API key from Constants.
-    static func post<T: Encodable, U: Decodable>(url: String, body: T, completionHandler: @escaping (U?) -> Void) {
-        guard let url = URL(string: url) else { return }
+    static func post<T: Encodable, U: Decodable>(url: String, body: T) async throws -> U {
+        guard let url = URL(string: url) else {
+            throw HTTPError.invalidURL
+        }
         
         var request = URLRequest(url: url)
         request.httpMethod = HTTPMethods.POST.rawValue
         request.setValue(Constants.API.apiKey, forHTTPHeaderField: "Authorization")
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        do {
-            let encoder = JSONEncoder()
-            encoder.keyEncodingStrategy = .convertToSnakeCase
-            request.httpBody = try encoder.encode(body)
-        } catch {
-            print("Error encoding request body: \(error)")
-            completionHandler(nil)
-            return
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        request.httpBody = try encoder.encode(body)
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode) else {
+            throw HTTPError.invalidResponse
         }
         
-        let sharedSession = URLSession.shared
-        
-        let dataTask = sharedSession.dataTask(with: request) {
-            (data, response, error) in
-                do {
-                    if let jsonData = data {
-                        let decoder = JSONDecoder()
-                        decoder.keyDecodingStrategy = .convertFromSnakeCase
-                        let typedObject: U? = try decoder.decode(U.self, from: jsonData)
-                        completionHandler(typedObject)
-                    } else {
-                        completionHandler(nil)
-                    }
-                }
-                catch {
-                    print("Error decoding response: \(error)")
-                    completionHandler(nil)
-                }
-        }
-        
-        dataTask.resume()
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode(U.self, from: data)
     }
 }
 
